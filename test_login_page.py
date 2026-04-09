077821033bf449cacae597b7f05b9f88973605df
Yuri Gorbunov <yurg824@gmail.com>
2026-04-09 22:07:12 +0300

    Добавлен класс LoginPage для проверки страницы логина и регистрации

---
+++ test_login_page.py
@@ -0,0 +1,7 @@
+from pages.login_page import LoginPage
+
+def test_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
