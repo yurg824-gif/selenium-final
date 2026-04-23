e2eb5e190192de3d6f3ee4ff7055b889bb39457c
Yuri Gorbunov <yurg824@gmail.com>
2026-04-23 21:24:59 +0300

    fix: conftest.py

--- conftest.py
+++ conftest.py
@@ -2,6 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+def pytest_addoption(parser):	
+	parser.addoption('--language', action='store', default="en", help="Choose language")
+
+
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
